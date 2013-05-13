90a2d23a35b711752d4025aa405b6637385696e9
Hayden Smith <mail@chambalam.com>
2013-05-14 09:14:46 +1000

    week 10

--- week9/src/Telephone.java
+++ week9/src/Telephone.java
@@ -91,21 +91,7 @@ public class Telephone extends Applet
     		   
     	   }
     	   gridY++;
-       }
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+       }       
        
        c.gridwidth = 3;
        
@@ -157,80 +143,7 @@ public class Telephone extends Applet
  	  gridbag.setConstraints(button, c);
  	  add(button);
    }
-		   
-/*		  
-      JPanel speakerPanel = new JPanel();
-      speakerPanel.setLayout(new BorderLayout());
-      speakerPanel.add(new JLabel("Speaker:"),
-            BorderLayout.NORTH);
-      speakerField = new JTextArea(10, 25);
-      speakerPanel.add(speakerField,
-            BorderLayout.CENTER);
-
-      String keyLabels = "123456789*0#";
-      JPanel keyPanel = new JPanel();
-      keyPanel.setLayout(new GridLayout(4, 3));
-      for (int i = 0; i < keyLabels.length(); i++)
-      {
-         final String label = keyLabels.substring(i, i + 1);
-         JButton keyButton = new JButton(label);
-         keyPanel.add(keyButton);
-         keyButton.addActionListener(new
-            ActionListener()
-            {
-               public void actionPerformed(ActionEvent event)
-               {
-                  connect.dial(label);
-               }
-            });
-      }
-      
-     
-
-      final JTextArea microphoneField = new JTextArea(10,25);
-
-      JButton speechButton = new JButton("Send speech");
-      speechButton.addActionListener(new
-         ActionListener()
-         {
-            public void actionPerformed(ActionEvent event)
-            {
-               connect.record(microphoneField.getText());
-               microphoneField.setText("");
-            }
-         });
-
-      JButton hangupButton = new JButton("Hangup");
-      hangupButton.addActionListener(new
-         ActionListener()
-         {
-            public void actionPerformed(ActionEvent event)
-            {
-               connect.hangup();
-            }
-         });
-
-      JPanel buttonPanel = new JPanel();
-      buttonPanel.add(speechButton);
-      buttonPanel.add(hangupButton);
-
-      JPanel microphonePanel = new JPanel();
-      microphonePanel.setLayout(new BorderLayout());
-      microphonePanel.add(new JLabel("Microphone:"),
-            BorderLayout.NORTH);
-      microphonePanel.add(microphoneField, BorderLayout.CENTER);
-      microphonePanel.add(buttonPanel, BorderLayout.SOUTH);
-
-      JFrame frame = new JFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.add(speakerPanel, BorderLayout.NORTH);
-      frame.add(keyPanel, BorderLayout.CENTER);
-      frame.add(microphonePanel, BorderLayout.SOUTH);
-
-      frame.pack();
-      frame.setVisible(true);
-   }*/
-
+   
    /**
       Give instructions to the mail system user.
    */
